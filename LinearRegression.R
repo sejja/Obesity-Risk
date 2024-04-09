1d731655955cfd903b0e9e96ea8384f5f52bafab
aitorubieta2004@gmail.com <aitorubieta2004@gmail.com>
2024-04-09 23:52:28 +0200

    initial regression

---
+++ LinearRegression.R
@@ -0,0 +1,8 @@
+# Perform linear regression
+linear_regression_model <- lm(Weight ~ Age + Height + family_history_with_overweight, data = obesity_data)
+
+# Summarize the regression results
+summary(model)
+
+plot(obesity_data$Weight, fitted(linear_regression_model), main = "Actual vs. Predicted", xlab = "Actual Weight", ylab = "Predicted Weight")
+abline(0, 1, col = "red")  # Add a diagonal line for perfect prediction
--- LogisticRegression.R
+++ LogisticRegression.R
@@ -5,7 +5,7 @@ library(ResourceSelection)
 data <- obesity_data
 data$NObeyesdad <- ifelse(data$NObeyesdad == 'Overweight_Level_III', 1, 0)
 data <- data[complete.cases(data),]
-model <- glm(SCC~Height+Age+FCVC+FAF,family=binomial(),data=data)
+logistic_regression_model <- glm(SCC~Height+Age+FCVC+FAF,family=binomial(),data=data)
 lratiostat <- model$null.deviance-model$deviance
 dofstat <- model$df.null- model$df.residual
 pchisq(lratiostat,df=dofstat,lower.tail = FALSE)
