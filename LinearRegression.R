ceb5985312ab49c454dd739838c3705840a8cb9a
aitorubieta2004@gmail.com <aitorubieta2004@gmail.com>
2024-04-12 11:06:07 +0200

    linear_regression

--- LinearRegression.R
+++ LinearRegression.R
@@ -1,8 +1,27 @@
+
+
+data_lm <- obesity_data
+
+
+# Calculate the confident interval
+confint((linear_regression_model))
+
 # Perform linear regression
-linear_regression_model <- lm(Weight ~ Age + Height + family_history_with_overweight, data = obesity_data)
+linear_regression_model <- lm(Weight ~ Height + family_history_with_overweight + FCVC + FAF ,data = data_lm)
 
 # Summarize the regression results
-summary(linear_regression_model)
+anova(linear_regression_model)
 
+# Plot the predicted weight against the actual weight
 plot(obesity_data$Weight, fitted(linear_regression_model), main = "Actual vs. Predicted", xlab = "Actual Weight", ylab = "Predicted Weight")
-abline(0, 1, col = "red")  # Add a diagonal line for perfect prediction
+abline(0, 1, col = "red",)  # Add a diagonal line for perfect prediction
+
+
+
+# Let's check the heteroscedasticity
+plot(linear_regression_model,1)
+
+# Let's check the influential points with cooks distance
+plot(linear_regression_model,4)
+
+
